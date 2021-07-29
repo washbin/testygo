fb1b358c6d456e7e4306c9eba580383b5fc12e47
washbin <76929116+washbin@users.noreply.github.com>
2021-07-29 17:37:42 +0545

    initial commit

---
+++ 01hello/hello.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+const french = "French"
+const spanish = "Spanish"
+const frenchHelloPrefix = "Bonjour, "
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/washbin/gotests
+
+go 1.16
